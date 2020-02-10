7ba59ff3cb74a7b41042d7cb63dbfffdd0497ff9
olivia2549 <ologanmail@gmail.com>
2020-02-10 10:29:01 -0600

    new project

---
+++ SmartIdeas/SmartIdeas/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SmartIdeas
+//
+//  Created by Olivia Logan on 2/10/20.
+//  Copyright © 2020 Olivia Logan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
