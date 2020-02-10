01f540852c560c21e93dcb6ae0ee92370ba3d8bb
olivia2549 <ologanmail@gmail.com>
2020-02-10 11:09:15 -0600

    table view cell

---
+++ SmartIdeas/SmartIdeas/SceduleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SceduleTableViewCell.swift
+//  SmartIdeas
+//
+//  Created by Olivia Logan on 2/10/20.
+//  Copyright © 2020 Olivia Logan. All rights reserved.
+//
+
+import UIKit
+
+class SceduleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
